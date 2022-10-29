20f6328888596268e659c7a0dab24f0be5a470f7
FusernameC <88580614+FusernameC@users.noreply.github.com>
2022-10-29 22:25:05 +0700

    Login prepare

---
+++ src/main/java/com/Login/LoginControl.java
@@ -0,0 +1,25 @@
+package com.Login;
+
+import java.io.IOException;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.store.MongoUtils;
+
+@WebServlet(name = "LoginControl", urlPatterns = {"/account"})
+
+
+public class LoginControl extends HttpServlet {
+    protected void processRequest (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String username = request.getParameter("user");
+        String password = request.getParameter("pass");
+
+    }    
+}
